105b37ee7235141074dde49c659769214a3fb5c4
Elix Merlino Neto <neto.elix@outlook.com>
2024-06-17 12:06:07 -0300

    Inserindo informações do player e renderizando na tela

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -1,4 +1,6 @@
 import tokenAndré from './tokens/TrishaMonroe.png';
+import tokenFernando from './tokens/HarperJammer.png';
+import tokenBruno from './tokens/SeanBean.png';
 
 export const data = [
   {
@@ -7,5 +9,19 @@ export const data = [
     profission: 'Comtech Marine',
     codname: 'Grey Weasel',
     images: tokenAndré,
+  },
+  {
+    id: 'Fernando (Gelatto)',
+    characterName: 'Harper "Offroad" Jammer',
+    profission: 'Piloto',
+    codname: 'Green Weasel',
+    images: tokenFernando,
+  },
+  {
+    id: 'Bruno',
+    characterName: 'Sean "Night Rider" Bean',
+    profission: 'Fuzileiro',
+    codname: 'Black Weasel',
+    images: tokenBruno,
   },
 ];
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,7 +1,35 @@
+import { data } from '../assets/data';
+
 function Player() {
   return (
     <div>
-      <h1>Player</h1>
+      {
+        data.map(({ id, characterName, profission, codname, images }) => (
+          <section key={ id }>
+            <h3>
+              Jogador:
+              {' '}
+              {id}
+            </h3>
+            <p>
+              Personagem:
+              {' '}
+              {characterName}
+            </p>
+            <p>
+              Profissão:
+              {' '}
+              {profission}
+            </p>
+            <p>
+              Codnome:
+              {' '}
+              {codname}
+            </p>
+            <img src={ images } alt={ characterName } />
+          </section>
+        ))
+      }
     </div>
   );
 }
