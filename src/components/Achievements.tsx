c5e09bd1f3ef1c0420865b19efe14b1c8bcb7fb8
Elix Merlino Neto <neto.elix@outlook.com>
2024-06-24 19:23:26 -0300

    Ajustando as conquistas

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -1,16 +1,14 @@
 import { AchievementsContainer } from '../styles/AchievementsStyle';
 import MedalIconBasic from '../assets/svg/MedalIconBasic.svg';
 
-interface Achievement {
-  description: string;
-  name: string;
-}
-
 interface AchievementsProps {
-  achievements: Achievement[];
+  achievements: Array<{
+    character: any; name: string, description: string
+  }>;
+  characterIndex: number;
 }
 
-function Achievements({ achievements }: AchievementsProps) {
+function Achievements({ achievements, characterIndex }: AchievementsProps) {
   return (
     <AchievementsContainer className="player-achievements">
       <div className="info-achievements">
@@ -18,6 +16,7 @@ function Achievements({ achievements }: AchievementsProps) {
           <div key={ index } className="achievements">
             <h3>{achievement.name}</h3>
             <p>{achievement.description}</p>
+            <p>{achievement.character[characterIndex].date}</p>
             <div className="achievements-img">
               <img src={ MedalIconBasic } alt="" />
             </div>
